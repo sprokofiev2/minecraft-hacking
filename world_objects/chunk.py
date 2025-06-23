6abc8ecc2d899fd046c6c68589a06d831d0d76f5
Sergei <asherkalfus20@gmail.com>
2025-06-23 16:48:48 -0400

    frustum culling

--- world_objects/chunk.py
+++ world_objects/chunk.py
@@ -12,6 +12,9 @@ class Chunk:
         self.voxels: np.array = None
         self.mesh: ChunkMesh = None
         self.is_empty = True
+
+        self.center = (glm.vec3(self.position) + 0.5) * CHUNK_SIZE
+        self.is_on_frustum = self.app.player.frustum.is_on_frustum
     
     def get_model_matrix(self):
         m_model = glm.translate(glm.mat4(), glm.vec3(self.position) * CHUNK_SIZE)
@@ -24,7 +27,7 @@ class Chunk:
         self.mesh = ChunkMesh(self)
     
     def render(self):
-        if not self.is_empty:
+        if not self.is_empty and self.is_on_frustum(self):
             self.set_uniform()
             self.mesh.render()
     
